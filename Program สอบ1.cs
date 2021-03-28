ae06f63cb0e783df3bbbc8dd8f16b3f174838a7c
thlyn01441048 <liyana.ptpc@mail.kmutt.ac.th>
2021-03-28 20:46:14 +0700

    new file

---
+++ Program สอบ1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp3_สอบจ่ะสอบ_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("input number:");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("input number:");
+            b = int.Parse(Console.ReadLine());
+
+            int x = a;
+            int y = b;
+
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else
+                {
+                    y = y + b;
+                }
+            }
+            Console.WriteLine(x);
+
+            Console.ReadLine();
+        }
+    }
+}
